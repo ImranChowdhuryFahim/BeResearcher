b1eabae324cd84f0bf8678948b0cbc81fdb11098
mateen <admatinm@gmail.com>
2020-09-28 08:22:06 +0600

    implement basic routing between home and admin-panel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
-import CourseSection from './CourseSection/App'
-import {CourseProvider} from './data'
+import React from "react";
+import "./App.css";
+import CourseSection from "./CourseSection/App";
+import AdminPanel from "./admin-panel";
+import { CourseProvider } from "./data";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <CourseProvider>
-    <div className="App">
-      <CourseSection></CourseSection>
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <div className="App">
+              <CourseSection></CourseSection>
+            </div>
+          </Route>
+          <Route path="/admin">
+            <AdminPanel />
+          </Route>
+        </Switch>
+      </Router>
     </CourseProvider>
   );
 }
