be0d97e71f6ca23236e8f5bb10861faec844b60e
mateen <admatinm@gmail.com>
2020-10-22 23:38:13 +0600

    make api call for create-assignment

--- src/admin-panel/body-section/CreateAssignment.js
+++ src/admin-panel/body-section/CreateAssignment.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./bodysection.css";
 
 import { CourseContext } from "../../data";
+import { faThinkPeaks } from "@fortawesome/free-brands-svg-icons";
 
 class CreateAssignment extends React.Component {
   constructor(props) {
@@ -9,10 +10,15 @@ class CreateAssignment extends React.Component {
     this.state = {
       title: "",
       body: "",
+      unit: "",
+      courseContent: {},
+      courseTitle: "",
+      isSubmitting: false,
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.handleUnitChange = this.handleUnitChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleTitleChange(event) {
@@ -21,23 +27,73 @@ class CreateAssignment extends React.Component {
   handleBodyChange(event) {
     this.setState({ body: event.target.value });
   }
+  handleUnitChange(event) {
+    this.setState({ unit: event.target.value });
+  }
   handleSubmit(event) {
-    const { title, body } = this.state;
-    console.log(title, body);
-    this.context.updateAssignment(1);
     event.preventDefault();
+    const { title, body, unit, courseTitle } = this.state;
+    console.log(this.state);
+    const newCourseContent = this.state.courseContent;
+    newCourseContent[unit].push({
+      unit: Number(unit),
+      title: title,
+      description: body,
+      type: "assignment",
+    });
+    console.log(newCourseContent);
+    this.setState({ isSubmitting: true });
+    fetch("https://beresearcherbd.herokuapp.com/api/course/updatecontent", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        courseTitle: courseTitle,
+        courseContent: newCourseContent,
+      }),
+    })
+      .then(() => {
+        alert("Assignment Suceesfully Created");
+        this.setState({ title: "", body: "", unit: "", isSubmitting: false });
+      })
+      .catch(() => {
+        console.log("Oops, something went wrong. Try creating again.");
+        this.setState({ title: "", body: "", unit: "", isSubmitting: false });
+      });
+    // this.context.updateAssignment(1);
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://beresearcherbd.herokuapp.com/api/course/getcoursedata/Research%20Methodology"
+    )
+      .then((resp) => resp.json())
+      .then((resp) => {
+        this.setState(
+          () => ({
+            courseContent: resp.courseContent,
+            courseTitle: resp.courseTitle,
+          }),
+          () => {
+            window.courseContent = this.state.courseContent;
+            window.courseTitle = this.state.courseTitle;
+          }
+        );
+      });
   }
 
   render() {
+    const { title, body, unit, isSubmitting } = this.state;
     return (
       <div className="create-assignment">
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="unit">Select Unit</label>
-          <select name="unit" id="unit">
+          <select name="unit" id="unit" onChange={this.handleUnitChange}>
             <option value="">Choose a unit</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
+            {Object.keys(this.state.courseContent).map((_, i) => (
+              <option>{i + 1}</option>
+            ))}
           </select>
           <label htmlFor="title">
             Title:
@@ -62,9 +118,17 @@ class CreateAssignment extends React.Component {
             />
           </label>
 
-          <input type="submit" value="Submit" />
+          <button
+            type="submit"
+            disabled={title === "" || body === "" || unit === ""}
+          >
+            {isSubmitting ? (
+              <div className="loading">Creating</div>
+            ) : (
+              <div>Create</div>
+            )}
+          </button>
         </form>
-        {/* <pre>{JSON.stringify(this.context.CourseContent, null, 4)}</pre> */}
       </div>
     );
   }
