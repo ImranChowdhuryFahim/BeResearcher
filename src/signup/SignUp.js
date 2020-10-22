a75f8bf3301a038f64f5217d39189031f03fcaab
mateen <admatinm@gmail.com>
2020-10-22 23:40:04 +0600

    build loading state in button when waiting for server response

--- src/signup/SignUp.js
+++ src/signup/SignUp.js
@@ -3,6 +3,7 @@ import { Formik, Form, ErrorMessage, useField } from "formik";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link, matchPath } from "react-router-dom";
+import { useHistory } from "react-router";
 import * as Yup from "yup";
 import logo from "./logo.png";
 import "./style.css";
@@ -130,6 +131,7 @@ export const SuperVisorSignUp = () => {
 };
 
 const SignUp = () => {
+  const history = useHistory();
   const [signUpAsSupervisor, setSignUpAsSupervisor] = useState(false);
   const [studentSingUp, setStudentSingUp] = useState(false); // for resposive view, less than 801px
 
@@ -156,7 +158,14 @@ const SignUp = () => {
         <img src={logo} alt="logo"></img>
       </div>
       <Formik
-        initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          institution: "",
+          occupation: "",
+        }}
         validationSchema={Yup.object({
           firstName: Yup.string()
             .max(15, "Must be 15 characters or less")
@@ -175,67 +184,94 @@ const SignUp = () => {
           //   .required("Required"),
           // occupation: Yup.string().required("Required"),
         })}
-        onSubmit={async (values, { setSubmitting }) => {
+        onSubmit={(values, { setSubmitting }) => {
           console.log(values);
-          await new Promise((r) => setTimeout(r, 500));
-          setSubmitting(false);
+          fetch(
+            "https://beresearcherbd.herokuapp.com/api/student/registration",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(values),
+            }
+          )
+            .then((result) => {
+              setSubmitting(false);
+              history.push("/dashboard");
+              return result.text();
+            })
+            .then((res) => console.log(res, "bhaiaja"))
+            .catch((err) => {
+              alert(err);
+              setSubmitting(false);
+            });
+          // await new Promise((r) => setTimeout(r, 500));
         }}
       >
-        <div className="student-signup-container">
-          <Form>
-            <h2>Student SignUp</h2>
-            <MyTextInput
-              label="First Name"
-              name="firstName"
-              type="text"
-              placeholder="Abdul"
-            />
-            <MyTextInput
-              label="Last Name"
-              name="lastName"
-              type="text"
-              placeholder="Matin"
-            />
-            <MyTextInput
-              label="Email"
-              name="email"
-              type="email"
-              placeholder="abdulmatin@gmail.com"
-            />
-            <MyTextInput
-              label="Password"
-              name="password"
-              type="password"
-              placeholder="********"
-            />
-            <MyTextInput
-              label="Institution"
-              name="institution"
-              type="text"
-              list="institution"
-              placeholder="Type a institution"
-            />
-            <datalist id="institution">
-              <option value="BUET">BUET</option>
-              <option value="CUET">CUET</option>
-              <option value="KUET">KUET</option>
-              <option value="RUET">RUET</option>
-              <option value="SUST">SUST</option>
-              <option value="DU">DU</option>
-              <option value="CU">CU</option>
-              <option value="RU">RU</option>
-              <option value="KU">kU</option>
-            </datalist>
-            <MySelect label="Occupation" name="occupation">
-              <option value="">Select a job type</option>
-              <option value="teacher">Teacher</option>
-              <option value="undergrad">Under Grad Student</option>
-              <option value="postgrad">Post Grad Student</option>
-            </MySelect>
-            <br />
-            <button type="submit">Submit</button>
-          </Form>
-        </div>
+        {(props) => (
+          <div className="student-signup-container">
+            <Form>
+              <h2>Student SignUp</h2>
+              <MyTextInput
+                label="First Name"
+                name="firstName"
+                type="text"
+                placeholder="Abdul"
+              />
+              <MyTextInput
+                label="Last Name"
+                name="lastName"
+                type="text"
+                placeholder="Matin"
+              />
+              <MyTextInput
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="abdulmatin@gmail.com"
+              />
+              <MyTextInput
+                label="Password"
+                name="password"
+                type="password"
+                placeholder="********"
+              />
+              <MyTextInput
+                label="Institution"
+                name="institution"
+                type="text"
+                list="institution"
+                placeholder="Type a institution"
+              />
+              <datalist id="institution">
+                <option value="BUET">BUET</option>
+                <option value="CUET">CUET</option>
+                <option value="KUET">KUET</option>
+                <option value="RUET">RUET</option>
+                <option value="SUST">SUST</option>
+                <option value="DU">DU</option>
+                <option value="CU">CU</option>
+                <option value="RU">RU</option>
+                <option value="KU">kU</option>
+              </datalist>
+              <MySelect label="Occupation" name="occupation">
+                <option value="">Select a job type</option>
+                <option value="teacher">Teacher</option>
+                <option value="undergrad">Under Grad Student</option>
+                <option value="postgrad">Post Grad Student</option>
+              </MySelect>
+              <br />
+              <button type="submit" disabled={props.isSubmitting}>
+                {props.isSubmitting ? (
+                  <div className="loading">Singing Up</div>
+                ) : (
+                  <div>Sign Up</div>
+                )}
+              </button>
+            </Form>
+          </div>
+        )}
       </Formik>
       <div className="supervisor-signup-container">
         <SuperVisorSignUp />
