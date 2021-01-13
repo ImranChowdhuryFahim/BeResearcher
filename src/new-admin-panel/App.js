158cd1a9c48b7bb0103f355b9e025971025c20b1
mateen <admatinm@gmail.com>
2021-01-13 21:10:57 +0600

    fix newadmin route-problems

--- src/new-admin-panel/App.js
+++ src/new-admin-panel/App.js
@@ -1,20 +1,41 @@
-import React, {Component} from 'react'
-import Navbar from './navbar/Navbar'
-import Sidebar from './sidebar/Sidebar'
-import Mainwindow from './mainwindow/Mainwindow'
-import './App.css'
+import React, { Component } from 'react';
+import Navbar from './navbar/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './menues/Dashboard/Dashboard';
+import Sidebar from './sidebar/Sidebar';
+import Mainwindow from './mainwindow/Mainwindow';
+// import './mainwindow/Mainwindow.css';
+import routes from './Routes';
+import './App.css';
 
-class App extends Component{
-    render()
-    {
-        return(
-            <div>
-                <Mainwindow></Mainwindow>
-                <Sidebar></Sidebar>
-                
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div style={{ display: 'flex' }}>
+        <BrowserRouter>
+          <Sidebar />
+          <div className="admin-mainwindow">
+            <Switch>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  children={<route.body />}
+                />
+              ))}
+
+              {/* <Route
+                path="/newadmin/dashboard"
+                exact={true}
+                children={() => <Dashboard />}
+              /> */}
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
