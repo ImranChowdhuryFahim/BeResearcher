6d94051d0aa545082e7c16c353b7d55aa7227dd0
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-01-18 15:03:48 +0600

    admin panel dashboard course-create

--- src/new-admin-panel/menues/Blog/Blog.js
+++ src/new-admin-panel/menues/Blog/Blog.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
-import './Blog.css'
+import React, { Component } from "react";
+import "./Blog.css";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState , convertToRaw} from "draft-js";
+import draftToHtml from "draftjs-to-html";
 
-class Blog extends Component{
-    render()
-    {
-        return(
-            <h1>Blog</h1>
-        )
-    }
+class Blog extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  };
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+  render() {
+    const { editorState } = this.state;
+    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+    return (
+      <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+        <div className="blog_write">
+          <Editor
+            editorState={this.state.editorState}
+            toolbarClassName="toolbarClassName"
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+            onEditorStateChange={this.onEditorStateChange}
+            placeholder="Write Here"
+            toolbar={{
+                image: {uploadEnabled: true, previewImage: false},
+            }}
+          />
+        </div>
+
+        <button className="post-button">Post</button>
+      </div>
+    );
+  }
 }
 
-export default Blog
+export default Blog;
--- src/new-admin-panel/menues/Course/Course.js
+++ src/new-admin-panel/menues/Course/Course.js
@@ -8,6 +8,7 @@ class Course extends Component{
           CourseContent: {
             1: [{ type: 'lecture' }],
           },
+          id: 0,
         };
       }
       handleCreateUnit()
@@ -39,13 +40,12 @@ class Course extends Component{
          newContent[unit][id]['id']=parseInt(rid)
          newContent[unit][id]['unit']=parseInt(unit)
          newContent[unit][id]['title']=event.target.value
-         newContent[unit][id]['type']='lecture'
          this.setState({CourseContent: newContent})
          //console.log(this.state.CourseContent)
       }
       handlesource(event)
       {
-        // console.log(event.target.value)
+        //  console.log(event.target.value)
         let unit,id,rid;
          unit=event.target.getAttribute('data-unit')
          id=event.target.getAttribute('data-id')
@@ -54,8 +54,59 @@ class Course extends Component{
          newContent[unit][id]['id']=parseInt(rid)
          newContent[unit][id]['unit']=parseInt(unit)
          newContent[unit][id]['src']=event.target.value
-         newContent[unit][id]['type']='lecture'
          this.setState({CourseContent: newContent})
+      }
+      handleDescription(event)
+      {
+        let unit,id,rid;
+         unit=event.target.getAttribute('data-unit')
+         id=event.target.getAttribute('data-id')
+         rid=event.target.getAttribute('data-rid')
+         let newContent=this.state.CourseContent
+         newContent[unit][id]['id']=parseInt(rid)
+         newContent[unit][id]['unit']=parseInt(unit)
+         newContent[unit][id]['description']=event.target.value
+         this.setState({CourseContent: newContent})
+        //  console.log(newContent)
+      }
+      handleQuestion(event)
+      {
+        let unit,id,rid,quiz;
+        unit=event.target.getAttribute('data-unit')
+        id=event.target.getAttribute('data-id')
+        rid=event.target.getAttribute('data-rid')
+        quiz=event.target.getAttribute('data-no')
+        let newContent=this.state.CourseContent
+        newContent[unit][id]['id']=parseInt(rid)
+        newContent[unit][id]['unit']=parseInt(unit)
+        newContent[unit][id]['question']=event.target.value
+        newContent[unit][id]['quiz_no']=parseInt(quiz)
+        this.setState({CourseContent: newContent})
+      }
+      handleOption(event)
+      {
+        let unit,id,rid,op;
+        unit=event.target.getAttribute('data-unit')
+        id=event.target.getAttribute('data-id')
+        rid=event.target.getAttribute('data-rid')
+        op=event.target.getAttribute('data-option')
+        let newContent=this.state.CourseContent
+        newContent[unit][id]['id']=parseInt(rid)
+        newContent[unit][id]['unit']=parseInt(unit)
+        newContent[unit][id]['option'+parseInt(op)]=event.target.value
+        this.setState({CourseContent: newContent})
+      }
+      handleAnswer(event)
+      {
+        let unit,id,rid;
+        unit=event.target.getAttribute('data-unit')
+        id=event.target.getAttribute('data-id')
+        rid=event.target.getAttribute('data-rid')
+        let newContent=this.state.CourseContent
+        newContent[unit][id]['id']=parseInt(rid)
+        newContent[unit][id]['unit']=parseInt(unit)
+        newContent[unit][id]['answer']=event.target.value
+        this.setState({CourseContent: newContent})
       }
       handleAddAssignment(event){
         let unit=event.target.getAttribute('data-key')
@@ -86,7 +137,9 @@ class Course extends Component{
         let newContent=this.state.CourseContent
         newContent[unit].splice(id,1)
         this.setState({CourseContent: newContent})
-        id--;
+        if(id>0){
+        this.setState({id: this.state.id-1})
+        }
 
       }
       handleDeleteUnit(event)
@@ -99,7 +152,7 @@ class Course extends Component{
       }
     render()
     {
-        let id=0;
+        let id=this.state.id;
         let lec=0;
         let assignment=0;
         let quiz=0;
@@ -135,7 +188,7 @@ class Course extends Component{
                   this.state.CourseContent[unit].map((lecture,i)=> {
                     id++;
                     
-                    console.log(lecture)
+                    // console.log(lecture)
                     if(lecture.type==='lecture')
                     {
                         lec++;
@@ -144,8 +197,8 @@ class Course extends Component{
 
                         <div key={id} className={'sections'}>
                             <div style={{ paddingBottom: '5px' }}>Lecture {lec}</div>
-                            <div style={{ display: 'flex' , gridGap: '10px', fontSize:'14px'}}>
-                           <div>Title <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. Introduction'} style={{ padding:'10px', width:'100%'}}></input></div> 
+                            <div style={{ padding:'5px', fontSize:'14px'}}>
+                           <div>Title <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. Introduction'} style={{marginBottom:'5px', padding:'10px', width:'100%'}}></input></div> 
                            <div>Source <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handlesource.bind(this)} placeholder={'Youtube Link'} style={{ padding:'10px',width:'100%'}}></input></div>
                            </div>
                            <button data-unit={unit} data-id={i}  onClick={this.handledelete.bind(this)} style={{marginTop:'5px',padding:'5px'}}>Delete</button>
@@ -162,9 +215,9 @@ class Course extends Component{
                        return(
                         <div key={id} className={'sections'}>
                            <div style={{ paddingBottom: '5px' }}> Assignment {assignment} </div>
-                           <div style={{ display: 'flex' , gridGap: '10px', fontSize:'14px'}}>
-                          <span>Title <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. Assignment1'} style={{ padding:'10px', width:'100%'}}></input></span>  
-                          <span>  Description <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handlesource.bind(this)} placeholder={'e.g. Assignment1'}  style={{ padding:'10px', width:'100%'}}></input></span>  
+                           <div style={{ padding:'5px', fontSize:'14px'}}>
+                          <div>Title <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. Assignment1'} style={{ marginBottom:'5px',padding:'10px', width:'100%'}}></input></div>  
+                          <div>  Description <textarea data-unit={unit} data-id={i} data-rid={id} onChange={this.handleDescription.bind(this)} placeholder={'e.g. Assignment1'}  style={{ padding:'10px', width:'100%'}}></textarea></div>  
                             </div>
                             <button data-unit={unit} data-id={i}  onClick={this.handledelete.bind(this)} style={{marginTop:'5px',padding:'5px'}}>Delete</button>
                         </div>                       
@@ -178,15 +231,18 @@ class Course extends Component{
                            <div key={id} className={'sections'}>
                                <div style={{ paddingBottom: '5px' }}> Quiz {quiz} </div>
                                <div>
-                                   <span>Question <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. What is the Capital of Bangladesh'} style={{ padding:'10px', width:'100%'}}></input></span>
+                                   <span>Question <input data-unit={unit} data-id={i} data-rid={id} data-no={quiz} onChange={this.handleQuestion.bind(this)} placeholder={'e.g. What is the Capital of Bangladesh'} style={{ padding:'10px', width:'100%'}}></input></span>
                                </div>
                                <div style={{ display: 'flex' , gridGap: '10px', fontSize:'14px',paddingTop:'5px'}}>
-                          <span>Option 1<input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. Dhaka'} style={{ padding:'10px', width:'100%'}}></input></span>  
-                          <span>Option 2<input data-unit={unit} data-id={i} data-rid={id} onChange={this.handlesource.bind(this)} placeholder={'e.g. Chittagont'}  style={{ padding:'10px', width:'100%'}}></input></span> 
+                          <span>Option 1<input data-unit={unit} data-id={i} data-rid={id} data-option={1} onChange={this.handleOption.bind(this)} placeholder={'e.g. Dhaka'} style={{ padding:'10px', width:'100%'}}></input></span>  
+                          <span>Option 2<input data-unit={unit} data-id={i} data-rid={id} data-option={2} onChange={this.handleOption.bind(this)} placeholder={'e.g. Chittagont'}  style={{ padding:'10px', width:'100%'}}></input></span> 
                           </div>
                           <div style={{ display: 'flex' , gridGap: '10px', fontSize:'14px',paddingTop:'5px'}}>
-                          <span>Option 3<input data-unit={unit} data-id={i} data-rid={id} onChange={this.handletitle.bind(this)} placeholder={'e.g. Comilla'} style={{ padding:'10px', width:'100%'}}></input></span>  
-                          <span>Option 4<input data-unit={unit} data-id={i} data-rid={id} onChange={this.handlesource.bind(this)} placeholder={'e.g. Barisal'}  style={{ padding:'10px', width:'100%'}}></input></span> 
+                          <span>Option 3<input data-unit={unit} data-id={i} data-rid={id} data-option={3} onChange={this.handleOption.bind(this)} placeholder={'e.g. Comilla'} style={{ padding:'10px', width:'100%'}}></input></span>  
+                          <span>Option 4<input data-unit={unit} data-id={i} data-rid={id} data-option={4} onChange={this.handleOption.bind(this)} placeholder={'e.g. Barisal'}  style={{ padding:'10px', width:'100%'}}></input></span> 
+                            </div>
+                            <div style={{display:'flex', flexDirection:'column', paddingTop: '5px'}}>
+                              Correct Answer <input data-unit={unit} data-id={i} data-rid={id} onChange={this.handleAnswer.bind(this)} placeholder={'e.g. Dhaka'} style={{ padding:'10px', width:'35%'}}></input>
                             </div>
                             <button data-unit={unit} data-id={i}  onClick={this.handledelete.bind(this)} style={{marginTop:'5px',padding:'5px'}}>Delete</button>
                            </div>
@@ -206,7 +262,7 @@ class Course extends Component{
         <button onClick={this.handleCreateUnit.bind(this)} className={'create-unit-button'}>Create Unit</button>
                     </div>
                 </div>
-                <input type="submit"></input>
+                <input type="submit" onClick={this.handleCreate.bind(this)}></input>
             </div>
         )
     }
