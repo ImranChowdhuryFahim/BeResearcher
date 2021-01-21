075e070c09278877831edd762dc57522cf373ceb
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-01-22 00:54:44 +0600

    css correction

--- src/CourseSection/Header/Header.js
+++ src/CourseSection/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import './Header.css'
-import logo from './logo2.png'
+import logo from './flogo.png'
 import ham from './ham.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faBullhorn } from '@fortawesome/free-solid-svg-icons'
@@ -19,13 +19,13 @@ class Header extends Component {
             <div>
                 <div className="CourseHeader">
                     <div className="LogoCourseSection">
-                        <img src={logo} alt="logo" style={{ height: '45px', marginTop: '2px', marginLeft: '35px' }}></img>
+                        <img src={logo} alt="logo" style={{ height: '60px', marginLeft: '35px' }}></img>
                     </div>
                     <div className="ham">
-                        <img src={ham} alt="hame" style={{ height: '45px', marginTop: '2px', marginLeft: '5px' }} onClick={this.props.toggole}></img>
+                        <img src={ham} alt="hame" style={{ height: '45px', marginLeft: '5px' }} onClick={this.props.toggole}></img>
                     </div>
                     <div className="ProgressBar" style={{ padding: '3.25px 13px 0px 0px' }}>
-                        <div className="ProgressLabel" style={{ marginTop: '5px', fontSize: '13px', fontWeight: 'bold' }}>
+                        <div className="ProgressLabel" style={{  fontSize: '13px', fontWeight: 'bold' }}>
                             <span style={{ color: '#31c984' }}>
                                 {((this.context.currentCourseProgress.completedItem/this.context.totalItem)*100).toFixed(1) + '%'} complete
                        </span>
@@ -37,13 +37,13 @@ class Header extends Component {
                         </div>
                     </div>
                     <div className="Announcement" style={{ display: 'flex' }}>
-                        <FontAwesomeIcon className="ans" icon={faBullhorn} size={'2x'} style={{ marginTop: '5px' }}></FontAwesomeIcon>
-                        <span className="ans" style={{ paddingLeft: '5px', fontSize: '13px', marginTop: '15px' }}>Announcements</span>
+                        <FontAwesomeIcon className="ans" icon={faBullhorn} size={'2x'} ></FontAwesomeIcon>
+                        <span className="ans" style={{ paddingLeft: '5px', fontSize: '13px', marginTop: '12px' }}>Announcements</span>
                     </div>
                     <div className="UserDetails">
                         <div className="UserProfile" style={{ display: 'flex', alignItems: 'center' }}>
-                            <FontAwesomeIcon icon={faUser} size={'2x'} style={{ marginTop: '5px' }}></FontAwesomeIcon>
-                            <span style={{ paddingLeft: '5px', fontSize: '11px', marginTop: '10px' }}>{this.context.CurrentUserDetails.name}</span>
+                            <FontAwesomeIcon icon={faUser} size={'2x'} ></FontAwesomeIcon>
+                            <span style={{ paddingLeft: '5px', fontSize: '11px' }}>{this.context.CurrentUserDetails.name}</span>
 
                         </div>
                         <div className="dropdown-content">
