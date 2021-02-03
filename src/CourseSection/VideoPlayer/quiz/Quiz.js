4ef8808511681fc2686dc51e5a65b151f2ea4f28
mateen <admatinm@gmail.com>
2021-02-03 22:56:29 +0600

    implement multiple question support in quiz

--- src/CourseSection/VideoPlayer/quiz/Quiz.js
+++ src/CourseSection/VideoPlayer/quiz/Quiz.js
@@ -2,62 +2,83 @@ import React, { useEffect, useState } from 'react';
 import './quiz.css';
 
 const Question = ({
-  question,
-  correctAnswer,
-  option1,
-  option2,
-  option3,
-  option4,
+  quiz,
+  // question,
+  // quiz.answer,
+  // option1,
+  // option2,
+  // option3,
+  // option4,
   OnQuizAnswered,
 }) => {
   const [showAnswer, setShowAnswer] = useState(false);
   const [options, setOptions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   const handleCheckingAnswer = (answer) => {
     console.log('chosen answer: ', answer);
     if (!showAnswer) {
       setShowAnswer(true);
     }
-    if (answer === correctAnswer) OnQuizAnswered();
+    // if (answer === quiz[currentQuestionIndex].answer) OnQuizAnswered();
   };
 
   const handleNextQuestion = () => {
-    // setCurrentQuestionIndex(currentQuestionIndex + 1);
-    // setShowAnswer(false);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setShowAnswer(false);
   };
 
   useEffect(() => {
-    setOptions([option1, option2, option3, option4]);
-  }, []);
+    if (quiz) {
+      setOptions([
+        quiz[currentQuestionIndex].option1,
+        quiz[currentQuestionIndex].option2,
+        quiz[currentQuestionIndex].option3,
+        quiz[currentQuestionIndex].option4,
+      ]);
+    }
+    console.log('******', quiz);
+  }, [quiz, currentQuestionIndex]);
 
   return (
+    // <div>
+    //   {!quiz ? 'kkkkkkkkkkkkk' : <div>{JSON.stringify(quiz, null, 2)}</div>}
+    // </div>
+
     <>
-      <div className="question-answer-container">
-        <div className="question">{question}</div>
-        <div className="answer-container">
-          {options.map((elem, index) => (
-            <button
-              className={
-                'answer ' +
-                (showAnswer
-                  ? elem === correctAnswer
-                    ? 'green-border'
-                    : 'red-border'
-                  : '')
-              }
-              onClick={() => handleCheckingAnswer(elem)}
-              dangerouslySetInnerHTML={{ __html: `${index + 1}. ${elem}` }}
-            />
-          ))}
+      {!quiz ? (
+        'loading...'
+      ) : (
+        <div className="question-answer-container">
+          <div className="question">{quiz[currentQuestionIndex].question}</div>
+          <div className="answer-container">
+            {options.map((elem, index) => (
+              <button
+                className={
+                  'answer ' +
+                  (showAnswer
+                    ? elem === quiz[currentQuestionIndex].answer
+                      ? 'green-border'
+                      : 'red-border'
+                    : '')
+                }
+                onClick={() => handleCheckingAnswer(elem)}
+                dangerouslySetInnerHTML={{ __html: `${index + 1}. ${elem}` }}
+              />
+            ))}
+          </div>
+          <button
+            className={
+              'next-btn ' +
+              (showAnswer && currentQuestionIndex < quiz.length ? '' : 'hidden')
+            }
+            title="go to next question"
+            onClick={handleNextQuestion}
+          >
+            NEXT
+          </button>
         </div>
-        {/* <button
-          className={'next-btn ' + (showAnswer ? '' : 'hidden')}
-          title="go to next question"
-          onClick={handleNextQuestion}
-        >
-          NEXT
-        </button> */}
-      </div>
+      )}
     </>
   );
 };
